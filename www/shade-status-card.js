ffe51ec0cb6a237b95012f6f70fcefb915cf19c9
William Zink <williamdzink@gmail.com>
2019-09-12 15:11:48 -0400

    Incremental UI changes for custom card

--- www/shade-status-card.js
+++ www/shade-status-card.js
@@ -2,40 +2,107 @@
 
 //You can use Polymer, Angular, Preact or any other popular framework
 
-class ShadeStatusCard extends HTMLElement {
-    set hass(hass) {
-      if (!this.content) {
-        const card = document.createElement('ha-card');
-        card.header = 'Example card';
-        this.content = document.createElement('div');
-        this.content.style.padding = '0 16px 16px';
-        card.appendChild(this.content);
-        this.appendChild(card);
-      }
-  
-      const entityId = this.config.entity;
-      const state = hass.states[entityId];
-      const stateStr = state ? state.state : 'unavailable';
-  
-      this.content.innerHTML = `
-        The state of ${entityId} is ${stateStr}!
-        <br><br>
-        <img src="http://via.placeholder.com/350x150">
-      `;
+
+//    const entityId = this.config.entity;
+//    const entityName = this.config.name;
+//    const state = hass.states[entityId];
+//    const stateStr = state ? state.state : 'unavailable';
+
+//    const blindClosedIcon = "mdi:blinds"
+//    const blindOpenIcon = "mdi:blinds-open";
+//    const arrowUpIcon = "mdi:arrow-up-bold";
+//    const arrowDownIcon = "mdi:arrow-down-bold";
+import { LitElement, html, css } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+
+class ShadeStatusCard extends LitElement {
+  static get properties() {
+    return {
+      _hass: {},
+      config: {},
+      entity: {},
+      stateString: String,
+      name: String
+    };
+  }
+
+  set hass(hass) {
+    if (!hass) return;
+    const entity = this.config.entity;
+    const name = this.config.name;
+    const entityState = hass.states[entity];
+    const stateString = entityState ? entityState.state : 'unavailable';
+    this._hass = hass;
+    if (stateString && this.stateString !== stateString) {
+      this.stateString = stateString;
     }
-  
-    setConfig(config) {
-      if (!config.entity) {
-        throw new Error('You need to define an entity');
-      }
-      this.config = config;
+    if (entity && this.entity !== entity) {
+      this.entity = entity;
     }
-  
-    // The height of your card. Home Assistant uses this to automatically
-    // distribute all cards over the available columns.
-    getCardSize() {
-      return 3;
+    if (name && this.name !== name) {
+      this.name = name;
     }
   }
-  
-  customElements.define('shade-status-card', ShadeStatusCard);
+
+  get hass() {
+    return this._hass;
+  }
+
+  render() {
+    return html`
+      <ha-card>
+        <div>
+          Hi Mom
+        </div>
+        ${this.entity}
+        <br>
+        ${this.stateString}
+        <br>
+        ${this.name}
+        <br>
+        <ha-icon id="down-arrow" icon="mdi:arrow-down-bold"></ha-icon> | <ha-icon id="down-arrow" icon="mdi:arrow-down-bold"></ha-icon>
+      </ha-card>
+    `;
+  }
+
+  renderUpArrow() {
+    const arrowUpIcon = "mdi:arrow-up-bold";
+
+    return html`
+      <div>
+        <ha-icon data-domain="arrow-up-bold" id="up-arrow" icon="mdi:arrow-up-bold"></ha-icon>
+      </div>
+      `;
+  }
+
+  renderDownArrow() {
+    const arrowDownIcon = "mdi:arrow-down-bold";
+
+    return html`<ha-icon id="down-arrow" icon="mdi:arrow-down-bold"></ha-icon>`;
+  }
+
+  renderShadeIcon() {
+     const blindClosedIcon = "mdi:blinds"
+     const blindOpenIcon = "mdi:blinds-open";
+     let icon = "";
+     if(this.stateString === "Closed") {
+       icon = blindClosedIcon;
+     } else {
+       icon = blindOpenIcon;
+     }  
+
+     return html`<ha-icon .icon=${icon}></ha-icon>`
+  }
+
+  getCardSize() {
+    return 2;
+  }
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    this.config = config;
+  }
+}
+
+customElements.define('shade-status-card', ShadeStatusCard);
