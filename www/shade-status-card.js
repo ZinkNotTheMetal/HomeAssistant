3a4d158456d47789b286d64a45447bae990cdd30
William Zink <williamdzink@gmail.com>
2020-06-16 23:45:43 -0500

    Update readme for latest version Fixed status card to follow convention

--- www/shade-status-card.js
+++ www/shade-status-card.js
@@ -112,7 +112,7 @@ class ShadeStatusCard extends LitElement {
 
   _allShadesUp() {
     this.hass.callService("ifttt", "trigger", {
-      "event": "all_shades_up_request"
+      "event": "all_shades_up"
     });
     this.hass.callService("input_boolean", "turn_on", {
       "entity_id": "input_boolean.blind_override"
@@ -121,7 +121,7 @@ class ShadeStatusCard extends LitElement {
 
   _allShadesDown() {
     this.hass.callService("ifttt", "trigger", {
-      "event": "all_shades_down_request"
+      "event": "all_shades_down"
     });
     this.hass.callService("input_boolean", "turn_on", {
       "entity_id": "input_boolean.blind_override"
